ddbf91402e8be2dad0183942b0aa7174346b5151
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-08-21 18:11:22 -0300

    Agrego maquetado inicial

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import MercurioApp from './MercurioApp'
+
 ReactDOM.render(
-  <App />,
+  <MercurioApp />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
