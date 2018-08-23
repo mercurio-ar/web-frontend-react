db0cab57ebbf5c2093bf243632a82c6e43bc3df4
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-08-22 23:19:02 -0300

    Agrego accion set current visualization

--- src/actions/currentVisualizationActions.ts
+++ src/actions/currentVisualizationActions.ts
@@ -1,6 +1,17 @@
 import { Action } from "redux";
+
+import { SET_CURRENT_VISUALIZATION } from "../constants/currentVisualizationConstants";
+
 import { IVisualization } from "../models/Visualization";
+
 
 export interface ICurrentVisualizationAction extends Action {
     visualization: IVisualization;
+}
+
+export function setCurrentVisualization(visualization: IVisualization): ICurrentVisualizationAction {
+    return {
+        type: SET_CURRENT_VISUALIZATION,
+        visualization
+    };
 }
