1250cc4d54e37ad0c1ef5f1ada8a0518496f4c40
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-08-30 19:31:14 -0300

    Agrego visualizationscontroler

--- src/MercurioApp.tsx
+++ src/MercurioApp.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import './MercurioApp.css'
 
+import VisualizationsController from './controllers/VisualizationsController';
+
 import NavBar from './components/NavBar';
 import SideNav from './components/SideNav/SideNav';
 import Visualization from './components/Visualization/Visualization';
@@ -15,6 +17,7 @@ class MercurioApp extends React.Component {
     public render() {
         return (
             <div>
+                <VisualizationsController />
                 <NavBar/>
 
                 <Container fluid={true}>
