e7b772265f6468519a526a997a3b86b192288a71
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-01 18:13:27 -0300

    Agrego search reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -4,18 +4,21 @@ import { IVisualization } from "../models/Visualization";
 
 import apis, { IApisState } from "./apisReducer";
 import currentVisualization from "./currentVisualizationReducer";
+import search, { ISearchState } from './searchReducer';
 import visualizations, { IVisualizationsState } from "./visualizationReducer";
 
 
 export interface IStoreState {
     apis: IApisState;
     currentVisualization: IVisualization["id"] | null;
+    search: ISearchState;
     visualizations: IVisualizationsState;
 }
 
 const rootReducer: Reducer<IStoreState, AnyAction> = combineReducers({
     apis,
     currentVisualization,
+    search,
     visualizations,
 });
 
---
+++ src/reducers/searchReducer.ts
@@ -0,0 +1,21 @@
+import { ISearchAction } from "../actions";
+import { ISearchQuery, ISearchResult } from "../apis";
+import { SET_SEARCH_ERROR, SET_SEARCH_RESULTS, SET_SEARCHING } from "../constants";
+import { searchInitialState } from "../initialStates";
+
+
+export interface ISearchState {
+    error: string | null;
+    searching: boolean;
+    results: ISearchResult[];
+    query: ISearchQuery;
+}
+
+export default function searchReducer(state = searchInitialState, action: ISearchAction){
+    switch(action.type){
+        case SET_SEARCHING: return {...state, searching: action.searching};
+        case SET_SEARCH_RESULTS: return {...state, results: action.results}
+        case SET_SEARCH_ERROR: return {...state, error: action.error}
+        default: return state;
+    }
+}
