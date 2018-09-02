a24ddb58fe51e081dd8150aa87e36ddaf423a41f
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-02 10:18:36 -0300

    Agrego modulo delete translations

---
+++ src/translations/ActionsTranslations/DeleteVisualizationActionTranslations.ts
@@ -0,0 +1,5 @@
+export const DeleteVisualizationActionTranslations = {
+    
+};
+
+export default DeleteVisualizationActionTranslations;
--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,1 +1,2 @@
 export * from './GlobalTranslations';
+export * from './ActionsTranslations';
