49ebd1cf457405fb7bf2cecc301d6791855d0699
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-25 10:17:07 -0300

    Added search modal to add serie action

--- src/components/Visualization/Header/Actions/VisualizationAddSerieAction.tsx
+++ src/components/Visualization/Header/Actions/VisualizationAddSerieAction.tsx
@@ -1,29 +1,48 @@
 import * as React from 'react';
 import { Translate } from 'react-localize-redux';
 
+import { SerieSearchModal } from '../../../SerieSearch';
 import { Button } from '../../../style';
 
 
 type IVisualizationAddSerieActionProps = React.Props<any>
 
-export class VisualizationAddSerieAction extends React.Component<IVisualizationAddSerieActionProps> {
+interface IVisualizationAddSerieActionState {
+    isModalOpen: boolean;
+}
 
-    constructor(props: IVisualizationAddSerieActionProps){
+export class VisualizationAddSerieAction extends React.Component<IVisualizationAddSerieActionProps, IVisualizationAddSerieActionState> {
+
+    constructor(props: IVisualizationAddSerieActionProps) {
         super(props);
 
+        this.state = {
+            isModalOpen: false,
+        };
+
         this.handleClick = this.handleClick.bind(this);
+        this.openSearchModal = this.openSearchModal.bind(this);
+        this.closeSearchModal = this.closeSearchModal.bind(this);
     }
 
-    public handleClick(event: React.SyntheticEvent<any>){
+    public handleClick(event: React.SyntheticEvent<any>) {
         event.stopPropagation();
-        // tslint:disable-next-line:no-console
-        console.log('sarras')
+        this.openSearchModal();
+    }
+
+    public openSearchModal() {
+        this.setState({ isModalOpen: true })
+    }
+
+    public closeSearchModal() {
+        this.setState({ isModalOpen: false })
     }
 
     public render() {
-        return(
+        return (
             <Button variant="outline-secondary" onClick={this.handleClick}>
-                <Translate id="Visualization.Actions.AddSerie"/>
+                <Translate id="Visualization.Actions.AddSerie" />
+                <SerieSearchModal open={this.state.isModalOpen} onClose={this.closeSearchModal} />
             </Button>
         );
     }
