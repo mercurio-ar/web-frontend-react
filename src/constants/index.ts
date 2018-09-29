275bc810e59bda6f55f1b1c585b7ead5c0458c75
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-29 09:47:48 -0300

    Added update visualizations constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export * from './apisContstants';
+export * from './apisConstants';
 export * from './currentVisualizationConstants';
 export * from './visualizationConstants';
 export * from './searchConstants';
