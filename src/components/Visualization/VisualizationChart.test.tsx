a310776dd19f18e90e1ed1038f6ab87a9f04167a
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-29 13:13:58 -0300

    Added tests for vischart data convertion

--- src/components/Visualization/VisualizationChart.test.tsx
+++ src/components/Visualization/VisualizationChart.test.tsx
@@ -32,4 +32,24 @@ describe('series => chartData adapter', () => {
             { name: "2018-01-02", serie1: 2, serie2: 1 },
         ]);
     });
+
+    it('translates series data to chart data', () => {
+        // Setup
+        const series: ISerie[] = [
+            { id: 1, displayName: "serie1", points: [{ x: "2018-01-01", y: 1 }, { x: "2018-01-02", y: 2 }] },
+            { id: 2, displayName: "serie2", points: [{ x: "2018-01-02", y: 2 }, { x: "2018-01-03", y: 1 }] },
+        ];
+        const props = { ...visCharProps, series };
+        const visChart = new VisualizationChart(props);
+
+        // Exercise
+        const actual = visChart.seriesData();
+
+        // Verify
+        expect(actual).toEqual([
+            { name: "2018-01-01", serie1: 1 },
+            { name: "2018-01-02", serie1: 2, serie2: 2 },
+            { name: "2018-01-03", serie2: 1 },
+        ]);
+    });
 });
