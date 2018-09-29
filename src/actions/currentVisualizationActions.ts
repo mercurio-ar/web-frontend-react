5b32af814492d1d05ce026123c94499d7cd69bd9
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-29 09:45:30 -0300

    Added action to add series to current visualization

--- src/actions/currentVisualizationActions.ts
+++ src/actions/currentVisualizationActions.ts
@@ -1,8 +1,21 @@
-import { Action } from "redux";
+import { Action, Dispatch } from "redux";
 
-import { SET_CURRENT_VISUALIZATION } from "../constants/currentVisualizationConstants";
+import {
+    SET_CURRENT_VISUALIZATION,
+} from "../constants/currentVisualizationConstants";
 
+import { ISearchResult } from "../apis";
 import { IVisualization } from "../models/Visualization";
+import { IStoreState } from "../reducers";
+import { 
+    getCurrentVisualization, 
+    getMercurioApi 
+} from "../selectors";
+import {
+    setFetchingVisualizations, 
+    setVisualizationsError, 
+    updateVisualization,
+} from "./visualizationActions";
 
 
 export interface ICurrentVisualizationAction extends Action {
@@ -13,5 +26,26 @@ export function setCurrentVisualization(visualization: IVisualization["id"]): IC
     return {
         type: SET_CURRENT_VISUALIZATION,
         visualization
+    };
+}
+
+export function addSerieFromSearchResultToCurrentVisualization(searchResult: ISearchResult) {
+    return (dispatch: Dispatch, getState: () => IStoreState) => {
+        const state = getState();
+        const currentVisualization = getCurrentVisualization(state);
+        const mercurioApi = getMercurioApi(state);
+        setFetchingVisualizations(true);
+        if(!currentVisualization){
+            return Promise.reject('Can not add serie: No visualization currently open');
+        }
+        return mercurioApi.addSearchResultToVisualization(currentVisualization, searchResult)
+            .then((visualization: IVisualization) => {
+                dispatch(updateVisualization(visualization));
+                dispatch(setFetchingVisualizations(false));
+            })
+            .catch((err: string) => {
+                dispatch(setVisualizationsError(err));
+                dispatch(setFetchingVisualizations(false));
+            });
     };
 }
