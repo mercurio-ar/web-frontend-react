2663d7b68287797b01615fb8473828788601eee9
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-29 09:48:20 -0300

    Added update visualization handler to visualizations reducer

--- src/reducers/visualizationReducer.ts
+++ src/reducers/visualizationReducer.ts
@@ -4,7 +4,8 @@ import {
     REMOVE_VISUALIZATIONS,
     SET_FETCHING_VISUALIZATION,
     SET_USER_VISUALIZATIONS,
-    SET_VISUALIZATIONS_ERROR
+    SET_VISUALIZATIONS_ERROR,
+    UPDATE_VISUALIZATIONS
 } from "../constants";
 import { visualizationInitialState } from "../initialStates";
 import { IVisualization } from "../models";
@@ -44,6 +45,22 @@ export default function visualizationReducer(state = visualizationInitialState, 
                 ] 
             } 
         };
+        case UPDATE_VISUALIZATIONS: return {
+            ...state, 
+            visualizations: {
+                ...state.visualizations,
+                user: updateVisualizations(state.visualizations.user, action.visualizations),
+            }
+        };
         default: return state;
     }
+}
+
+function updateVisualizations(visualizations: IVisualization[], updatedVisualizations: IVisualization[]): IVisualization[] {
+    return visualizations.map((visualization: IVisualization) => {
+        const updated = updatedVisualizations.find((updatedVisualization) =>
+            visualization.id === updatedVisualization.id
+        );
+        return updated || visualization;
+    });
 }
